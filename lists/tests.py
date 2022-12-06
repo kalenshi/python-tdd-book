851df9cf3db1186ac31176e172f2446d80aa2b3b
kalenshi <kalenshi@gmail.com>
2022-12-06 08:43:14 -0400

    Updated the template and tests

--- lists/tests.py
+++ lists/tests.py
@@ -6,3 +6,11 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self) -> None:
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_post_request(self):
+        request_data = {"item_text": "A new list item"}
+        response = self.client.post("/", data=request_data)
+
+        self.assertIn(
+            request_data["item_text"], response.content.decode("utf8")
+        )
