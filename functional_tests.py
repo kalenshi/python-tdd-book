245dd394b091b3aafdd988ec975442f2bc07d8b9
kalenshi <kalenshi@gmail.com>
2022-12-06 23:54:58 -0400

    refactored functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text) -> None:
+        table = self.browser.find_element(value="id_list_table")
+        rows = table.find_elements(by=By.TAG_NAME, value="tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self) -> None:
         # Edith has heard about a cool new online to-do app. She goes
         # to check out it's homepage
@@ -38,27 +43,19 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
         # There is still a text box inviting her to add another item. She
         # enters  "Use peacock feathers to make a fly" (Edith is very methodical)
-        input_box = self.browser.find_element(By.ID, "id_new_item")
+        input_box = self.browser.find_element(value="id_new_item")
         input_box.send_keys("Use peacock feathers to make a fly")
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows]
-        )
         # Edith wonders whether the site will still remember her list. Then she sees
         # that the site generated a unique URL for her -- there is some
         # explanatory text to that effect.
