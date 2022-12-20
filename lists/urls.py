feb05e3c3f329ce9e4a7b3e1811b5900c52904dc
kalenshi <kalenshi@gmail.com>
2022-12-20 08:52:24 -0400

    seperated urls to app level instead of project level

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,9 @@
-# from django.urls import path, re_path
-#
-# from lists import views
-#
-# urlpatterns = [
-#     path("new", views.new_list, name="new_list"),
-#     re_path("the-only-list-in-the-world/", views.view_list, name="view_list"),
-# ]
+from django.urls import path
+
+from lists import views
+
+urlpatterns = [
+    path("new", views.new_list, name="new_list"),
+    path("<list_id>/", views.view_list, name="view_list"),
+    path("<list_id>/add_item", views.add_item, name="add_item"),
+]
--- superlists/urls.py
+++ superlists/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include
 
 from lists import views
+from lists import urls as list_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home_page, name="home"),
-    path("lists/new", views.new_list, name="new_list"),
-    path("lists/<list_id>/", views.view_list, name="new_list"),
-    path("lists/<list_id>/add_item", views.add_item, name="add_item"),
+    path("lists/", include(list_urls)),
+
 ]
